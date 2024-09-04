0b15ecace4ac64d6f2ebe7cb37b9e94d68c50bee
alzabirr <alzabirhussain@gmail.com>
2024-09-05 03:54:03 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/index', function () {
+    return view('about');
+});
